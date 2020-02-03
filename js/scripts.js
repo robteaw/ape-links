0c04e22c405e6b3d581fb6b3dff4f27f0b607544
Robert Tea <robteaw@gmail.com>
2020-02-03 00:37:07 -0600

    first

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Navbar Mobile
+$('.navTrigger').click(function () {
+  $(this).toggleClass('active');
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
+
+});
+
+
+// Scroll to top
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 300){
+      $('.goTop').css({
+        "opacity":"1", "pointer-events":"auto"
+      });
+    }else{
+      $('.goTop').css({
+        "opacity":"0", "pointer-events":"none"
+      });
+    }
+  });
+  $('.goTop').click(function(){
+    $('html').animate({scrollTop:0}, 500);
+  });
+});
