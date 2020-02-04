f5372204adb6045b96eedbee73fcb69dbf90d212
Robert Tea <robteaw@gmail.com>
2020-02-04 01:44:11 -0600

    added more sites

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,13 @@ $(document).ready(function(){
     $('html').animate({scrollTop:0}, 500);
   });
 });
+
+// Function used to shrink nav bar removing paddings and adding black background
+  $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+          $('.nav').addClass('affix');
+          console.log("OK");
+      } else {
+          $('.nav').removeClass('affix');
+      }
+  });
